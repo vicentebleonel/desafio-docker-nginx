15be31a17c00c4e71e58ead76fd3efa9cf778e93
Vicente Barros Leonel <35276873+vicentebleonel@users.noreply.github.com>
2022-01-05 11:24:30 -0300

    Delete setup.sql

--- setup.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE people ( nome VARCHAR(150) );
-insert into people(nome) values ('Vicente');
-insert into people(nome) values ('Lucas');
-insert into people(nome) values ('Andre');
-insert into people(nome) values ('Gustavo');

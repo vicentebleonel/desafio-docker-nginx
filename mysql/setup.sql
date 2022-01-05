b8dd85c4d20d4c62cd1b99b22bc471c60c6a4ece
Vicente Barros Leonel <35276873+vicentebleonel@users.noreply.github.com>
2022-01-05 11:25:43 -0300

    Add files via upload

---
+++ mysql/setup.sql
@@ -0,0 +1,5 @@
+CREATE TABLE people ( nome VARCHAR(150) );
+insert into people(nome) values ('Vicente');
+insert into people(nome) values ('Lucas');
+insert into people(nome) values ('Andre');
+insert into people(nome) values ('Gustavo');
